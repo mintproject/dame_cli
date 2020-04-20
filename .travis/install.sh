287c5624ee1b82e9168b753cd97adc0493f66d2e
Maximiliano Osorio <maxiosorio@gmail.com>
2020-04-20 11:24:43 -0700

    cleaning: travis

--- .travis/install.sh
+++
@@ -1,7 +0,0 @@
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-    export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"'
-    source ~/venv/bin/activate
-else
-    echo "linux"
-fi
-
--- .travis/script.sh
+++
@@ -1,7 +0,0 @@
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-    source ~/venv/bin/activate
-    tox
-else
-    echo "linux"
-fi
-
