f461bfda458e4ce1e354a8ec80ec50eedf7521a2
Maximiliano Osorio <maxiosorio@gmail.com>
2020-04-20 15:07:05 -0700

    fix: test

--- src/dame/tests/test_cli_methods.py
+++ src/dame/tests/test_cli_methods.py
@@ -18,7 +18,7 @@ class Test(TestCase):
     def test_verify_input_parameters(self):
         runner = CliRunner()
         partial_setup = get_setup(SETUP_FULL_INFO)
-        assert verify_input_parameters(partial_setup) == partial_setup
+        assert verify_input_parameters(partial_setup, False) == partial_setup
 
     def test_print_data_property_table(self):
         for setup in setups:
