a7354ce38281f2cfb3b89ee9e1fad224bfe62a9b
Maximiliano Osorio <maxiosorio@gmail.com>
2020-05-04 12:18:56 -0700

    add test extract_zip

--- src/dame/tests/test_utils.py
+++ src/dame/tests/test_utils.py
@@ -1,9 +1,15 @@
 from unittest import TestCase
 
-from dame.utils import obtain_id
+from dame.utils import obtain_id, download_extract_zip
 
 
 class Test(TestCase):
     def test_obtain_id(self):
         url = "https://w3id.org/okn/i/mint/cycles-0.10.2-alpha-collection-oromia-single-point"
         assert obtain_id(url) == "cycles-0.10.2-alpha-collection-oromia-single-point"
+
+
+def test_download_extract_zip(tmp_path):
+    d = tmp_path / "sub"
+    zip_file = "https://github.com/mintproject/HAND-TauDEM/raw/v2.1.0/hand_v2_mint_component.zip"
+    download_extract_zip(zip_file, d)
