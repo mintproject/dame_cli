e1a2f0278f6733beb5468f6306692e66e5045058
Maximiliano Osorio <maxiosorio@gmail.com>
2020-06-01 10:17:27 -0700

    remove: setup_list

--- src/dame/tests/test___main__.py
+++ src/dame/tests/test___main__.py
@@ -1,12 +1,13 @@
 from unittest import TestCase
 
 from click.testing import CliRunner
+from modelcatalog import Configuration
 
 import dame
 from dame.__main__ import version, setup_show, setup_list, model_configuration_list, model_configuration_show, configure
 
 SETUP_FULL_INFO = "cycles-0.10.2-alpha-collection-oromia-single-point"
-
+USERNAME = "mint@isi.edu"
 
 class Test(TestCase):
     def test_version(self):
@@ -24,15 +25,16 @@ class Test(TestCase):
         result = runner.invoke(setup_show, "not_found")
         assert result.exit_code == 1
 
-    def test_setup_list(self):
-        runner = CliRunner()
-        result = runner.invoke(setup_list)
-        assert result.exit_code == 0
-
-    def test_model_configuration_list(self):
-        runner = CliRunner()
-        result = runner.invoke(model_configuration_list)
-        assert result.exit_code == 0
+    # def test_setup_list(self):
+    #     runner = CliRunner()
+    #     runner.invoke(configure, ["--server", "https://api.models.mint.isi.edu/v1.4.0", "--username", USERNAME])
+    #     result = runner.invoke(setup_list)
+    #     assert result.exit_code == 0
+    #
+    # def test_model_configuration_list(self):
+    #     runner = CliRunner()
+    #     result = runner.invoke(model_configuration_list)
+    #     assert result.exit_code == 0
 
     def test_model_configuration_show(self):
         runner = CliRunner()
